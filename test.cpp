3a97ebafb338af3d46e371d8bdd5e77a7848e789
qiuxer <127676957+qiuxer@users.noreply.github.com>
2023-03-12 23:33:06 +0800

    add a file

--- test.cpp
+++ test.cpp
@@ -1,4 +1,166 @@
-int main(){
-  cout<<"hhhh"<endl;
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+#include <iostream>
+//#include <string>
+using namespace std;
+
+int main() {
+	float a = 3e-4f;
+	cout << a << endl;
 }
-  
+
+
+
+
+//int main() {
+//	int a = 10;
+//	//=
+//	a = 100;
+//	cout << "a = " << a << endl;
+//	//+=
+//	a += 10;
+//	cout << "a = " << a << endl;
+//	//-=
+//	a -= 10;
+//	cout << "a = " << a << endl;
+//	///=
+//	a /= 2;
+//	cout << "a = " << a << endl;
+//	//*=
+//	a *= 10;
+//	cout << "a = " << a << endl;
+//	//%=
+//	a %= 2;
+//	cout << "a = " << a << endl;
+//
+//}
+
+
+
+
+
+
+
+
+
+
+//int main() {
+//
+//	//加
+//	int a = 10;
+//	int b = 20;
+//	cout << "加：" << a + b << endl;
+//	//减
+//	cout << "减：" << a - b << endl;
+//	//乘
+//	double c = 0.25;
+//	cout << "乘:" << a * b << endl;
+//	cout <<"乘1:" << a * c << endl;
+//	//除
+//	cout <<"除:" << a / b << endl;
+//	//取模
+//	cout <<"模:" << a % b << endl;
+//
+// }
+
+
+
+//int main() {
+//	
+//	////整型
+//	//int a;
+//	//cout << "请为a赋值：" << endl;
+//	//cin >> a;
+//	//cout << "a = " << a << endl;
+//
+//	////浮点型
+//	//float f;
+//	//cout << "请为f赋值：" << endl;
+//	//cin >> f;
+//	//cout << "f = " << f << endl;
+//
+//	//字符串
+//	string str;
+//	cout << "请为str字符串赋值：" << endl;
+//	cin >> str;
+//	cout << "str = " <<str<<endl;
+//
+//	system("pause");
+//	return 0;
+//
+//}
+
+
+
+
+
+
+
+
+//int main() {
+//
+//	bool a = (bool)43;
+//	bool b = (bool)0;
+//	cout << a <<endl;
+//	cout << b << endl;
+//
+/////*	cout << false << endl;
+////	cout << true << endl;
+////	cout <<"所占字节"<< sizeof(bool)*/ << endl;
+//
+//	system("pause");
+//	return 0;
+//
+//}
+
+
+//int main() {
+//	int a;
+//	cin >> a;
+//	cout << a << endl;
+//
+//	char str[] = "hello world";
+//
+//	string str1 = "hello world";
+//
+//	cout << str << endl;
+//	cout << str1 << endl;
+//
+//	system("pause");
+//	return 0;
+//
+//}
+
+
+
+
+//
+//int main() {
+//
+//	cout << "dfa\n" << endl;
+//	cout << "dfa\\" << endl;
+//
+//	cout << "asd\taf;anf;n "<<endl;
+//	cou*/t << "sdasda\tafdasdf;n " << endl;
+//}
+
+
+
+
+
+//int main() {
+//
+//	short a = 10;
+//	int b = 10;
+//	long c = 10;
+//	long long d = 10;
+//
+//	cout << "a 字节= " << sizeof(a) << endl;
+//	cout << "b 字节= " << sizeof(b) << endl;
+//	cout << "c 字节= " << sizeof(c) << endl;
+//	cout << "d 字节= " << sizeof(d) << endl;
+//
+//	system("pause");
+//	return 0;
+//}
